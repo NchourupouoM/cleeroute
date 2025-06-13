14fc501cb1ef96b360602c3980d244c781f7011e
NchourupouoM <nchourupouom04@gmail.com>
2025-06-13 17:25:33 +0100

    confuguring cors for allowing some origin access from localhost

--- src/cleeroute/main.py
+++ src/cleeroute/main.py
@@ -1,9 +1,35 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Request, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from src.cleeroute.crew import Course_structure_crew, Course_meta_datas_crew
 from src.cleeroute.models import CourseInput, Course_meta_datas_input
+from sqlalchemy.orm import Session
+from typing import List
 import time 
 
+# from db.models import VideoSearch, VideoResponse
+# from db.services import search_similar_videos
+# from db.db import get_db_connection
+
 app = FastAPI()
+
+# Configurer les origines autorisées
+origins = [
+    "http://localhost",
+    "http://localhost:3000",  # React   
+    "http://localhost:8000",  # FastAPI
+    "http://127.0.0.1",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"],
+)
+
+
 
 # python -m src.cleeroute.main  // pour lancer le serveure
 
@@ -46,6 +72,18 @@ def generate_course_structure(request: CourseInput):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     
+
+# ===================== get videos by category for each subsection ===============
+
+# @app.post("/search")
+# async def search_videos(request: dict):
+#     conn = get_db_connection()
+#     try:
+#         videos = search_similar_videos(request)
+#         return videos
+#     finally:
+#         conn.close()
+
 
 if __name__ == "__main__":
     import uvicorn
