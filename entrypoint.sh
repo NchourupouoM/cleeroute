7965d9646dab9ebce061ac0e168c49ae6d7e2254
NchourupouoM <nchourupouom04@gmail.com>
2025-11-17 15:38:18 +0100

    mod: modify conf file

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@ uvicorn src.cleeroute.main:app --host 0.0.0.0 --port 8000 &
 # Attendez que FastAPI soit prêt
 sleep 10
 
-# Démarrez Celery avec un utilisateur non-root
-celery -A worker worker --loglevel=info --uid=$(id -u)
+# Démarrez Celery sans changer d'utilisateur
+celery -A worker worker --loglevel=info --uid=0
